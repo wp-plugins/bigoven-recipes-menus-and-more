ca5907e8de64a9f877b7a5fa75929cab9e6a0a81
bigoven <bigoven@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-12 18:15:11 +0000

    Ziplist import, Rich Snippets SEO, Save Recipe, Save Grocery List features
    Please see http://wordpress.bigoven.com for release details
    
    git-svn-id: https://plugins.svn.wordpress.org/bigoven-recipes-menus-and-more/trunk@1024597 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ components/display/shortcode/shortcode.php
@@ -0,0 +1,83 @@
+<?php
+
+if(!defined('ABSPATH')) { exit; }
+
+// Recipes are stored as a custom post type with the following post_type key
+if(!defined('BO_RECIPES_RECIPE_SHORTCODE')) {
+	define('BO_RECIPES_RECIPE_SHORTCODE', 'seo_recipe');
+}
+
+class BO_Recipes_Components_Display_Shortcode {
+
+	/**
+	 * Initialize this component by setting up appropriate actions and filters,
+	 * as well as adding shortcodes as necessary and performing any default
+	 * tasks that are needed on site load.
+	 */
+	public static function init() {
+		self::_add_actions();
+		self::_add_filters();
+	}
+
+	private static function _add_actions() {
+		if(is_admin()) {
+			// Actions that only affect the administrative interface or operation
+		} else {
+			// Actions that only affect the frontend interface or operation
+			add_action('wp_enqueue_scripts', array(__CLASS__, 'enqueue_scripts'));
+		}
+
+		// Actions that affect both the administrative and frontend interface or operation
+		add_action('init', array(__CLASS__, 'add_shortcodes'));
+	}
+
+	private static function _add_filters() {
+		if(is_admin()) {
+			// Filters that only affect the administrative interface or operation
+		} else {
+			// Filters that only affect the frontend interface or operation
+		}
+
+		// Filters that affect both the administrative and frontend interface or operation
+	}
+
+	#region Shortcode Registration
+
+	public static function add_shortcodes() {
+		add_shortcode(BO_RECIPES_RECIPE_SHORTCODE, array(__CLASS__, 'display_recipe'));
+	}
+
+	#endregion Shortcode Registration
+
+	#region Shortcode Display
+
+	public static function display_recipe($atts, $content = null) {
+		$recipe_id = isset($atts['id']) && is_numeric($atts['id']) ? absint($atts['id']) : null;
+		$recipe = $recipe_id ? get_post($recipe_id) : null;
+
+		ob_start();
+
+		if($recipe && BO_RECIPES_RECIPE_TYPE === $recipe->post_type && 'publish' === $recipe->post_status) {
+			include(path_join(dirname(__FILE__), 'views/recipe.php'));
+		}
+
+		return ob_get_clean();
+	}
+
+	#endregion Shortcode Display
+
+	#region Scripts and Styles
+
+	public static function enqueue_scripts() {
+		if((is_singular() || is_page()) && has_shortcode(get_queried_object()->post_content, BO_RECIPES_RECIPE_SHORTCODE)) {
+			wp_enqueue_style('bo-recipes-shortcode', plugins_url('resources/bo-recipes-shortcode.css', __FILE__), array(), BO_RECIPES_VERSION);
+			wp_enqueue_script('bo-recipes-shortcode', plugins_url('resources/bo-recipes-shortcode.js', __FILE__), array('jquery'), BO_RECIPES_VERSION, true);
+		}
+	}
+
+	#endregion Scripts and Styles
+}
+
+require_once('lib/template-tags.php');
+
+BO_Recipes_Components_Display_Shortcode::init();
---
+++ readme.txt
@@ -0,0 +1,27 @@
+Alpha Release of BigOven Wordpress Plugin
+
+IMPORTANT
+* Before installing, be sure to update your Wordpress database.  
+* When you press the "Import from Ziplist" option in Settings, it will make irreversible changes to prior 
+posts, pointing them to the new recipe SEO recipes.  This is because you're migrating the recipes into the 
+new format, and we want to ensure that old posts are pointing to the new data format.
+
+How to back up your database:
+  http://codex.wordpress.org/Backing_Up_Your_Database
+
+HOW TO INSTALL
+* Back up your Wordpress database
+* Download the zip file
+* Extract the contents to your blog's /wp-content/plugins directory
+* Go to your wordpress dashboard (i.e., www.yourblog.com/wp-admin), look to the left, and click "Plugins"
+* Activate the BigOven Recipes plugin
+
+FEATURES
+* Recipe SEO - mark up recipes in rich snippet format
+* Import content from Ziplist format
+* Update older posts from Ziplist
+* Optional "Save Recipe" button allows your readers to make grocery lists and save recipes
+
+For more information, please visit http://wordpress.bigoven.com 
+
+
